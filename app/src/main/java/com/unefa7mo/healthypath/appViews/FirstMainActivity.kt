65f4f5682d1816ca78544c83af5e1dcc7f49bb0f
pc <gabrielmoises1202@gmail.com>
2025-03-26 23:33:22 -0400

    mychanges

---
+++ app/src/main/java/com/unefa7mo/healthypath/appViews/FirstMainActivity.kt
@@ -0,0 +1,21 @@
+package com.unefa7mo.healthypath.appViews
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.unefa7mo.healthypath.R
+
+class FirstMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_first_main)
+        Log.d("FirstMainActivity","onCreate")
+
+        }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("FirstMainActivity","onDestroy")
+        }
+    }
