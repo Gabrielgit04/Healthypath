4facc639ef606752f4ba2050d14c0c77e6aa82c2
pc <gabrielmoises1202@gmail.com>
2025-03-30 19:08:43 -0400

    mychanges

--- app/src/main/java/com/unefa7mo/healthypath/nutrition/NutritionViewActivity.kt
+++ app/src/main/java/com/unefa7mo/healthypath/nutrition/NutritionViewActivity.kt
@@ -2,7 +2,6 @@ package com.unefa7mo.healthypath.nutrition
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
