f72e9feec3f7bc670f10bcb7a52df1e6b31d60a3
pc <gabrielmoises1202@gmail.com>
2025-03-29 20:39:09 -0400

    mychanges

--- app/src/main/java/com/unefa7mo/healthypath/appViews/FirstMainActivity.kt
+++ app/src/main/java/com/unefa7mo/healthypath/appViews/FirstMainActivity.kt
@@ -1,9 +1,13 @@
 package com.unefa7mo.healthypath.appViews
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageButton
+
 import androidx.appcompat.app.AppCompatActivity
 import com.unefa7mo.healthypath.R
+import com.unefa7mo.healthypath.nutrition.NutritionViewActivity
 
 class FirstMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +16,19 @@ class FirstMainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_first_main)
         Log.d("FirstMainActivity", "onCreate")
 
+        val goNext = findViewById<ImageButton>(R.id.firstView)
+        goNext.setOnClickListener {
+            val intent = Intent(this, NutritionViewActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Log.d("FirstMainActivity", "onDestroy")
     }
+
 
 }
--- app/src/main/java/com/unefa7mo/healthypath/nutrition/NutritionViewActivity.kt
+++ app/src/main/java/com/unefa7mo/healthypath/nutrition/NutritionViewActivity.kt
@@ -1,4 +1,4 @@
-package com.unefa7mo.healthypath.appViews
+package com.unefa7mo.healthypath.nutrition
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.unefa7mo.healthypath.R
 
-class secondViewActivity : AppCompatActivity() {
+class NutritionViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_second_view)
+        setContentView(R.layout.activity_nutrition_view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
