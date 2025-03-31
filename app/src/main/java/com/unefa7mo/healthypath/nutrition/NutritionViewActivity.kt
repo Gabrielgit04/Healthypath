6662c1767698c77e6baf7c417fc4859136dc9c5f
pc <gabrielmoises1202@gmail.com>
2025-03-30 22:59:02 -0400

    new changes

--- app/src/main/java/com/unefa7mo/healthypath/nutrition/NutritionViewActivity.kt
+++ app/src/main/java/com/unefa7mo/healthypath/nutrition/NutritionViewActivity.kt
@@ -1,28 +1,29 @@
 package com.unefa7mo.healthypath.nutrition
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.unefa7mo.healthypath.R
 import com.unefa7mo.healthypath.appViews.FirstMainActivity
 
 class NutritionViewActivity : AppCompatActivity() {
+    private lateinit var backbutton: ImageButton
+    private lateinit var peso: EditText
+    private lateinit var altura: EditText
+    private lateinit var submit: androidx.appcompat.widget.AppCompatButton
+    private lateinit var texto: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nutrition_view)
 
-        initcomponents()
-        pressbutton()
-
     }
 
-    private lateinit var backbutton: ImageButton
-    private lateinit var peso: EditText
-    private lateinit var altura: EditText
-    private lateinit var submit: androidx.appcompat.widget.AppCompatButton
 
 
     private fun initcomponents() {
@@ -30,14 +31,9 @@ class NutritionViewActivity : AppCompatActivity() {
         peso = findViewById(R.id.peso)
         altura = findViewById(R.id.altura)
         submit = findViewById(R.id.submit)
+        texto =findViewById(R.id.description)
     }
 
-    private fun pressbutton() {
-        backbutton.setOnClickListener {
-            val intent = Intent(this, FirstMainActivity::class.java)
-            startActivity(intent)
-
-        }
-
     }
-}
+
+
