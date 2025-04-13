b8196b987dc135163296450a95209f2c89c37eef
pc <gabrielmoises1202@gmail.com>
2025-04-12 20:05:54 -0400

    mis cambios 12/04

--- app/src/main/java/com/unefa7mo/healthypath/activityphysique/dialogscontents/CalisteniaDialog.kt
+++ app/src/main/java/com/unefa7mo/healthypath/activityphysique/dialogscontents/CalisteniaDialog.kt
@@ -3,6 +3,9 @@ package com.unefa7mo.healthypath.activityphysique.dialogscontents
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
@@ -10,16 +13,30 @@ import com.unefa7mo.healthypath.R
 
 
 class CalisteniaDialog: DialogFragment() {
+
+    lateinit var  myView: View
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
         super.onCreate(savedInstanceState)
-        val myView =
-            requireActivity().layoutInflater.inflate(R.layout.activity_calistenia_dialog, null)
+        myView = requireActivity().layoutInflater.inflate(R.layout.activity_calistenia_dialog, null)
+        tablaRutina()
         return AlertDialog.Builder(requireContext()).setView(myView).setCancelable(false)
             .setPositiveButton("Salir", DialogInterface.OnClickListener{ dialog, which ->
                 dismiss()
             }).create()
 
+        }
+    lateinit var tabla: TableLayout
+    fun tablaRutina(){
+        tabla = myView.findViewById<TableLayout>(R.id.tablaex)
+        val fila1 = TableRow(requireContext())
+        val columna1 = TextView(requireContext())
+        columna1.text = "Lunes"
+        fila1.addView(columna1)
+        tabla.addView(fila1)
+
     }
+
 }
 
 
--- app/src/main/java/com/unefa7mo/healthypath/nutrition/DialogResult.kt
+++ app/src/main/java/com/unefa7mo/healthypath/nutrition/DialogResult.kt
@@ -2,7 +2,9 @@ package com.unefa7mo.healthypath.nutrition
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
@@ -15,48 +17,18 @@ class DialogResult : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
-
-        val result = arguments?.getDouble("imcResult")
-        if (result == null) {
+        val resultado = arguments?.getDouble("imcResult")
+        if (resultado == null) {
             return super.onCreateDialog(savedInstanceState)
         }
         val myVist = requireActivity().layoutInflater.inflate(R.layout.activity_dialog_result, null)
         val myResult = myVist.findViewById<TextView>(R.id.result)
-        myResult.text = result.toString()
-        return AlertDialog.Builder(requireContext()).setView(myVist).create()
+        myResult.text = resultado.toString()
+        return AlertDialog.Builder(requireContext()).setView(myVist).setCancelable(false).setPositiveButton("Salir", DialogInterface.OnClickListener{ dialog, which ->
+                dismiss()}).create()
 
-        fun changeColor(){
-
-            if (result < 18.5){
-                val boxResult = myVist.findViewById<LinearLayout>(R.id.result)
-                boxResult.setBackgroundResource(R.drawable.incorrect)
-            }
-
-            else if (result >= 18.5 && result <= 24.9){
-                val boxResult = myVist.findViewById<LinearLayout>(R.id.result)
-                boxResult.setBackgroundResource(R.drawable.correct)
-
-            }
-            else if (result >= 25 && result <= 29.9){
-                val boxResult = myVist.findViewById<LinearLayout>(R.id.result)
-                boxResult.setBackgroundResource(R.drawable.incorrect)
-            }
-
-        }
-
-        fun closeDialog(){
-            val accept = myVist.findViewById<TextView>(R.id.accept)
-                accept.setOnClickListener {
-                dismiss()
-            }
-        }
-
-            changeColor()
-            closeDialog()
-
-
-            }
     }
+}
 
 
 
