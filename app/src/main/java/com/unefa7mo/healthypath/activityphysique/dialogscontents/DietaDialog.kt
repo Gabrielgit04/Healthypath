83789dc814e791774c46c50a2413ec3c9713f740
pc <gabrielmoises1202@gmail.com>
2025-04-15 19:59:13 -0400

    mis cambios 15/04

--- app/src/main/java/com/unefa7mo/healthypath/activityphysique/dialogscontents/DietaDialog.kt
+++ app/src/main/java/com/unefa7mo/healthypath/activityphysique/dialogscontents/DietaDialog.kt
@@ -3,7 +3,10 @@ package com.unefa7mo.healthypath.activityphysique.dialogscontents
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.unefa7mo.healthypath.R
@@ -11,16 +14,30 @@ import com.unefa7mo.healthypath.R
 
 class DietaDialog: DialogFragment() {
 
+    private lateinit var pesoxg: EditText
+    private lateinit var calcularpxg: Button
     lateinit var myView: View
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
         super.onCreate(savedInstanceState)
         myView = requireActivity().layoutInflater.inflate(R.layout.activity_dieta_dialog, null)
+        calcularkgxp()
         return AlertDialog.Builder(requireContext()).setView(myView).setCancelable(false)
             .setPositiveButton("Salir", DialogInterface.OnClickListener { dialog, which ->
                 dismiss()
             }).create()
 
     }
+        fun calcularkgxp(){
+        pesoxg = myView.findViewById(R.id.pesoxg)
+        calcularpxg = myView.findViewById(R.id.calcularpxg)
+
+        calcularpxg.setOnClickListener {
+            val resultado = pesoxg.text.toString().toInt() * 2
+            Log.d("Resultado", resultado.toString())
+    }
+
+    }
+
 
 }
