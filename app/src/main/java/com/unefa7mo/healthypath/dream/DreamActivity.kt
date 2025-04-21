0f4e93f5a73e61e632cea3d4a8cd06c92d9b1df4
pc <gabrielmoises1202@gmail.com>
2025-04-20 20:36:55 -0400

    mis cambios 20/04, ya voy a terminar esta monda

--- app/src/main/java/com/unefa7mo/healthypath/dream/DreamActivity.kt
+++ app/src/main/java/com/unefa7mo/healthypath/dream/DreamActivity.kt
@@ -1,14 +1,28 @@
 package com.unefa7mo.healthypath.dream
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.unefa7mo.healthypath.R
+import com.unefa7mo.healthypath.appViews.FirstMainActivity
 
 class DreamActivity : AppCompatActivity() {
+    private lateinit var backbutton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dream)
+        backbutton = findViewById<ImageButton>(R.id.backbutton)
+        volver()
         }
 
+    fun volver() {
+
+        backbutton.setOnClickListener {
+            val intent = Intent(this, FirstMainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
     }
